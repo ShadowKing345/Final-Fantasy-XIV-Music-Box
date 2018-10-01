41bb4d7dda5cce9a3216c587d993648e575e31cc
Alex <alexandruturcanu915@gmail.com>
2018-10-01 10:37:37 +0300

    Created to remove copies

---
+++ Java/References.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.awt.*;
+
+public class References {
+
+    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    static Config config = new Config();
+    static Dimension size = new Dimension(650, 600);
+    static Font font = new Font("Bahnschrift", Font.PLAIN, 15);
+    static Play play = new Play();
+    static int DelayTime = 50;
+}
