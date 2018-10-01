08b0cf493b8b70a64149e5fe96496d1e5e8a0be6
Alex <alexandruturcanu915@gmail.com>
2018-10-01 11:11:52 +0300

    Added some design variables

--- Java/References.java
+++ Java/References.java
@@ -7,7 +7,12 @@ public class References {
     static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     static Config config = new Config();
     static Dimension size = new Dimension(650, 600);
-    static Font font = new Font("Bahnschrift", Font.PLAIN, 15);
     static Play play = new Play();
     static int DelayTime = 50;
+
+    static Color backgroundCol = new Color(35, 35, 35);
+    static Color innerPanelCol = new Color(40,40,40);
+    static Color componentsBGCol = new Color(55, 55, 55);
+    static Font font = new Font("Bahnschrift", Font.PLAIN, 15);
+    static Color textCol = new Color(228, 228, 228);
 }
